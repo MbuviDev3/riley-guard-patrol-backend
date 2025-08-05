b84e66accb2087f619d633c54db7dca0e95e983d
MbuviDev3 <mbuvidev@gmail.com>
2025-08-05 16:35:05 +0300

    Test push

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const userRoutes = require('./routes/')
+
 const app = express();
 app.use(cors());
 app.use(express.json());
